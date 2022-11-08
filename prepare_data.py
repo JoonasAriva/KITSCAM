d8a9f1a8d8c48bce57e9de070783c070e4768b95
JoonasAriva <joonasariva@gmail.com>
2022-11-08 10:12:59 +0200

    finsihed version of data preparation script

--- prepare_data.py
+++ prepare_data.py
@@ -50,33 +50,44 @@ for file in tqdm(segmentations):
     organized_slices[case_id] = [kidney_slices[::5], no_kidney_slices[::5]]
 
 np.random.seed(19)  # fix seed
-patient_indexes = np.arange(300)
+patient_indexes = list(organized_slices.keys())
 np.random.shuffle(patient_indexes)
-
+logging.info(str(len(patient_indexes)) + " patients")
 logging.info("splitting slice indexes to train/val(scorecam)/test")
-train_slices = get_slices(organized_slices, patient_indexes[:50], file_name='train_slices')
-scorecam_slices = get_slices(organized_slices, patient_indexes[50:250], file_name='scorecam_slices')
-test_slices = get_slices(organized_slices, patient_indexes[:50], file_name='test_slices')
+train_slices = get_slices(organized_slices, patient_indexes[:100], file_name='train_slices')
+scorecam_slices = get_slices(organized_slices, patient_indexes[100:250], file_name='scorecam_slices')
+test_slices = get_slices(organized_slices, patient_indexes[250:], file_name='test_slices')
+
+# use this block if slice indexes are already saved
+# with open("train_slices.json", "r") as read_file:
+#     train_slices = json.load(read_file)
+# with open("scorecam_slices.json", "r") as read_file:
+#     scorecam_slices = json.load(read_file)
+# with open("test_slices.json", "r") as read_file:
+#     test_slices = json.load(read_file)
 
 logging.info("collecting ct images based on indexes")
-for slice_type, slices in {'train': train_slices, 'scorecam': scorecam_slices, 'test': test_slices}:
+for slice_type, slices in {'train': train_slices, 'scorecam': scorecam_slices, 'test': test_slices}.items():
 
     logging.info('starting with ' + slice_type)
     firstiter = True
-    for (keys, values) in tqdm(slices.items()):
-        ct_scan = nib.load(keys + '/imaging.nii.gz').get_fdata()
-
+    for (key, value) in tqdm(slices.items()):
+        ct_scan = nib.load(key + '/imaging.nii.gz').get_fdata()
+        if ct_scan.shape[1:] != (512, 512):
+            logging.warning(
+                "dimensions {0} are not 512, 512 for patient {1}, skipping patient".format(ct_scan.shape[1:], key))
+            continue
         if firstiter:
-            all_kidney_slices = ct_scan[values[0]]
-            all_no_kidney_slices = ct_scan[values[1]]
+            all_kidney_slices = ct_scan[value[0]]
+            all_no_kidney_slices = ct_scan[value[1]]
             firstiter = False
         else:
-            new_kidney_slices = ct_scan[values[0]]
-            new_no_kidney_slices = ct_scan[values[1]]
+            new_kidney_slices = ct_scan[value[0]]
+            new_no_kidney_slices = ct_scan[value[1]]
             all_kidney_slices = np.concatenate((all_kidney_slices, new_kidney_slices), axis=0)
             all_no_kidney_slices = np.concatenate((all_no_kidney_slices, new_no_kidney_slices), axis=0)
 
-    logging.info('saving ' + slice_type + 'images')
+    logging.info('saving ' + slice_type + ' images')
     np.save(slice_type + 'kidney_slices', all_kidney_slices)
     np.save(slice_type + 'no_kidney_slices', all_no_kidney_slices)
 
