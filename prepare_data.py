6d249b19d36557cfc5c60babc05f01b630ff09b8
JoonasAriva <joonasariva@gmail.com>
2022-11-09 15:08:42 +0200

    updated data handling files

--- prepare_data.py
+++ prepare_data.py
@@ -29,50 +29,55 @@ def get_slices(data: Dict, patient_indexes: np.ndarray, file_name: str = None) -
 
 logging.basicConfig(format='%(asctime)s | %(levelname)s: %(message)s', level=logging.NOTSET)
 
-os.chdir('/gpfs/space/home/joonas97/data/kits21/kits21/data/')
-
+#os.chdir('/gpfs/space/home/joonas97/data/kits21/kits21/data/')
+os.chdir('/gpfs/space/home/joonas97/data/kits21/processed_2d_slices/')
 # gather all KITS segmentation files
 
-segmentations = []
-logging.info("collecting filenames")
-for filename in glob.glob('*/aggregated_MAJ_seg.nii.gz'):
-    segmentations.append(filename)
+# segmentations = []
+# logging.info("collecting filenames")
+# for filename in glob.glob('*/aggregated_MAJ_seg.nii.gz'):
+#     segmentations.append(filename)
 
 # load images and filter them to slices with and without kidney
-organized_slices = {}
-logging.info("organizing ct slice indexes to kidney/no kidney groups")
-for file in tqdm(segmentations):
-    case_id = file.split('/')[0]
-    segmentation = nib.load(file).get_fdata()
-    kidney_slices, no_kidney_slices = classify_slices(segmentation)
-
-    # use only every fifth slice (neighbouring slices are very similar)
-    organized_slices[case_id] = [kidney_slices[::5], no_kidney_slices[::5]]
-
-np.random.seed(19)  # fix seed
-patient_indexes = list(organized_slices.keys())
-np.random.shuffle(patient_indexes)
-logging.info(str(len(patient_indexes)) + " patients")
-logging.info("splitting slice indexes to train/val(scorecam)/test")
-train_slices = get_slices(organized_slices, patient_indexes[:100], file_name='train_slices')
-scorecam_slices = get_slices(organized_slices, patient_indexes[100:250], file_name='scorecam_slices')
-test_slices = get_slices(organized_slices, patient_indexes[250:], file_name='test_slices')
+# organized_slices = {}
+# logging.info("organizing ct slice indexes to kidney/no kidney groups")
+# for file in tqdm(segmentations):
+#     case_id = file.split('/')[0]
+#     segmentation = nib.load(file).get_fdata()
+#     kidney_slices, no_kidney_slices = classify_slices(segmentation)
+#
+#     # use only every fifth slice (neighbouring slices are very similar)
+#     organized_slices[case_id] = [kidney_slices[::5], no_kidney_slices[::5]]
+#
+# np.random.seed(19)  # fix seed
+# patient_indexes = list(organized_slices.keys())
+# np.random.shuffle(patient_indexes)
+# logging.info(str(len(patient_indexes)) + " patients")
+# logging.info("splitting slice indexes to train/val(scorecam)/test")
+# train_slices = get_slices(organized_slices, patient_indexes[:100], file_name='train_slices')
+# scorecam_slices = get_slices(organized_slices, patient_indexes[100:250], file_name='scorecam_slices')
+# test_slices = get_slices(organized_slices, patient_indexes[250:], file_name='test_slices')
 
 # use this block if slice indexes are already saved
 # with open("train_slices.json", "r") as read_file:
 #     train_slices = json.load(read_file)
-# with open("scorecam_slices.json", "r") as read_file:
-#     scorecam_slices = json.load(read_file)
-# with open("test_slices.json", "r") as read_file:
-#     test_slices = json.load(read_file)
+with open("scorecam_slices.json", "r") as read_file:
+    scorecam_slices = json.load(read_file)
+with open("test_slices.json", "r") as read_file:
+    test_slices = json.load(read_file)
 
+os.chdir('/gpfs/space/home/joonas97/data/kits21/kits21/data/')
 logging.info("collecting ct images based on indexes")
-for slice_type, slices in {'train': train_slices, 'scorecam': scorecam_slices, 'test': test_slices}.items():
+
+file_type = '/aggregated_MAJ_seg.nii.gz'  #'/imaging.nii.gz'
+
+logging.info('operating with filetype ' + file_type)
+for slice_type, slices in {'scorecam': scorecam_slices, 'test': test_slices}.items():
 
     logging.info('starting with ' + slice_type)
     firstiter = True
     for (key, value) in tqdm(slices.items()):
-        ct_scan = nib.load(key + '/imaging.nii.gz').get_fdata()
+        ct_scan = nib.load(key + file_type).get_fdata()
         if ct_scan.shape[1:] != (512, 512):
             logging.warning(
                 "dimensions {0} are not 512, 512 for patient {1}, skipping patient".format(ct_scan.shape[1:], key))
@@ -88,7 +93,7 @@ for slice_type, slices in {'train': train_slices, 'scorecam': scorecam_slices, '
             all_no_kidney_slices = np.concatenate((all_no_kidney_slices, new_no_kidney_slices), axis=0)
 
     logging.info('saving ' + slice_type + ' images')
-    np.save(slice_type + 'kidney_slices', all_kidney_slices)
-    np.save(slice_type + 'no_kidney_slices', all_no_kidney_slices)
+    np.save(slice_type + 'kidney_slices_seg', all_kidney_slices)
+    np.save(slice_type + 'no_kidney_slices_seg', all_no_kidney_slices)
 
 logging.info("All done!")
---
+++ utils.py
@@ -0,0 +1,42 @@
+import torch
+import numpy as np
+from torchvision import transforms
+
+
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(self, data_dir, mu=None, std=None):
+        super(CustomDataset, self).__init__()
+        # store the raw tensors
+
+        self._x = np.load(data_dir + '/data.npy')
+        self._y = np.load(data_dir + '/labels.npy')
+        print("data loaded")
+        print("Shape: ", self._x.shape)
+        if mu is None or std is None:
+            print("calculating mean and std")
+            self.mu = np.mean(self._x)
+            self.std = np.std(self._x)
+            print("std and mu calculated")
+        else:
+            self.mu = mu
+            self.std = std
+
+        self.compose = transforms.Compose([
+            transforms.Normalize((self.mu), (self.std))])
+        self.classes = ["no_kidney", "kidney"]
+
+    def __len__(self):
+        # a DataSet must know its size
+        return self._x.shape[0]
+
+    def __getitem__(self, index):
+
+        x = self._x[index, :]
+        x = torch.from_numpy(x)
+        y = self._y[index]
+
+        if len(x.shape) == 2:
+            x = torch.stack([x, x, x], 0)
+
+        x = self.compose(x)
+        return x, y
